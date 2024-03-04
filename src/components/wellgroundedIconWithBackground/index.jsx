d317e346e7abc56b47fbed2d1edcd9437219342b
Lukas Dembicki <lad_777@hotmail.com>
2024-03-04 17:50:33 +0000

    style: style for home page added

--- src/components/wellgroundedIconWithBackground/index.jsx
+++ src/components/wellgroundedIconWithBackground/index.jsx
@@ -1,5 +1,6 @@
 import wellGroundedIcon from "../../assets/svgs/wellgrounded-horizontal-background.svg"
 
+
 // eslint-disable-next-line react/prop-types
 function WellGroundedIconWithBackground({size}) {
   return (
