b30d2be1f5a0c4801e65c2f488ace83e4edbbe03
Lukas Dembicki <lad_777@hotmail.com>
2024-03-04 13:29:46 +0000

    build: added prospect routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+            <Route path="about" element={<About />} />
+            <Route path="my-approach" element={<MyApproach />} />
+            <Route path="services-and-fees" element={<ServicesAndFees />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
     </>
   )
 }
