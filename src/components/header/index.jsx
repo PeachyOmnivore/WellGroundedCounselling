c3627d9440ef9235dcca2c7f5f0cd32af44e41ac
Lukas Dembicki <lad_777@hotmail.com>
2024-03-04 17:08:06 +0000

    build: header component built and styled

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,11 @@
 import "./header.css"
+import WellGroundedIconWithBackground from "../../components/wellgroundedIconWithBackground/index.jsx"
 
 function Header () {
   return (
-    <>
-    
-    </>
+    <div className="header">
+      <WellGroundedIconWithBackground size="400px"/>
+    </div>
   )
 }
 
