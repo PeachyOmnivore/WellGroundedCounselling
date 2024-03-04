af9d3945ac4886719f108bffc260a1a3602ae256
Lukas Dembicki <lad_777@hotmail.com>
2024-03-04 13:39:15 +0000

    feat: imports for components included.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import MyApproach from './pages/myApproach'
+import ServicesAndFees from './pages/servicesAndFees'
+import Contact from './pages/contact'
+import PrivacyPolicy from './pages/privacyPolicy'
+
 
 function App() {
 
   return (
     <>
       <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="my-approach" element={<MyApproach />} />
             <Route path="services-and-fees" element={<ServicesAndFees />} />
