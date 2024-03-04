bba2dfbfb4a5d98c79882105f29c8efff3352e51
Lukas Dembicki <lad_777@hotmail.com>
2024-03-04 16:54:26 +0000

    feat: icon component created

---
+++ src/components/wellgroundedIconWithBackground/index.jsx
@@ -0,0 +1,9 @@
+import wellGroundedIcon from "../../assets/svgs/wellgrounded-horizontal-background.svg"
+
+function WellGroundedIconWithBackground() {
+  return (
+    <img src={wellGroundedIcon} width="300px" />
+  )
+}
+
+export default WellGroundedIconWithBackground
