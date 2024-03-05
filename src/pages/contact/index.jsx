b680b38d697057309fbf3e0095c70e53c14ef09d
Lukas Dembicki <lad_777@hotmail.com>
2024-03-05 17:27:45 +0000

    feat: started contact component

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -2,9 +2,11 @@ import "./contact.css"
 
 function Contact () {
   return (
-    <>
-    
-    </>
+    <div className="contact-container">
+    <form action="">
+      <input type="text" title="name" />
+    </form>
+    </div>
   )
 }
 
