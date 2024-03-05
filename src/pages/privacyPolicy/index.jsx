704808a8c574222dbe47e7396053e1e79fdc3f00
Lukas Dembicki <lad_777@hotmail.com>
2024-03-05 17:11:39 +0000

    feat: privacy policy component and styling

--- src/pages/privacyPolicy/index.jsx
+++ src/pages/privacyPolicy/index.jsx
@@ -1,9 +1,97 @@
 import "./privacyPolicy.css"
+import Header from "../../components/header"
+import Footer from "../../components/footer"
 
-function PrivacyPolicy () {
+function PrivacyPolicy() {
   return (
     <>
-    
+      <Header />
+      <div className="privacy-policy-container">
+        <h1>Privacy Policy</h1>
+        <p>Thank you for visiting Well Grounded Counselling. I am committed to protecting and respecting your privacy. This Privacy Policy outlines how I collect, use, and safeguard your personal data in accordance with the General Data Protection Regulation (GDPR).</p>
+
+        <h2>Contact details:</h2>
+        <ul>
+          <li>Name: Fiona Hutchison t/a Well Grounded Counseling</li>
+          <li>E-mail: <a href="mailto:fiona@wellgroundedcounselling.com">fiona@wellgroundedcounselling.com</a></li>
+          <li>ICO reference number: ZB635655</li>
+        </ul>
+
+        <h2>The type of personal information I collect</h2>
+        <strong>I collect information about you when you choose to enquire about my service:</strong>
+        <ul>
+          <li>Your name</li>
+          <li>Your contact details including your name, email address, your home or business address</li>
+        </ul>
+
+        <strong>I will collect further information if we choose to work together:</strong>
+        <ul>
+          <li>Date of birth</li>
+          <li>GP name/address</li>
+          <li>Emergency contact name</li>
+          <li>Address and phone number</li>
+        </ul>
+        <br />
+        <p>I sometimes take brief notes after the session, which will be held in accordance with GDPR. The notes are anonymised and contain no identifying information. I am not legaly obliged to take notes but if I have done so you have the right to see them if you wish.</p>
+
+        <h2>How I obtain the information:</h2>
+        <strong>Most of the personal information I process is provided to me directly by
+          you in one of the following ways:</strong>
+        <ul>
+          <li>When you send a contact form on this website (your name and email address)</li>
+          <li>When you enquire via one of the counselling directories I am listed on. </li>
+          <li>When you enquire via email (implied consent) or have agreed and wish to use any of my services.</li>
+        </ul>
+
+        <p>When we have agreed to work together, I will verbally ask you for your date of birth, GP name/ address, emergency contact name, address and phone number. </p>
+        <h2>How I will use this information:</h2>
+
+        <p>Any client data collected and/or kept by us, including personal information, is processed primary for the purpose of communication relevant to the delivery of a therapeutic counselling services. Additionally, this data is processed to fulfill any legal or professional obligations that arise in the provision of counselling services.</p>
+
+        <h2>Under the UK General Data Protection Regulation (UK GDPR), the lawful
+          bases we rely on for processing this information are:</h2>
+        <ul>
+          <li>We have a contractual obligation.</li>
+          <li>We have a legal obligation.</li>
+          <li>We have a legitimate interest.</li>
+        </ul>
+        <strong>There are a few instances where I would share information with a third party:</strong>
+        <ul>
+          <li>With Your Consent. With your explicit consent, I may disclose your information to other professionals or individuals whom you have specified or agreed that we should communicate with.</li>
+          <li>Prevention of Serious Harm. In situations where there is reasonable belief that sharing your information could prevent serious harm to you or another individual, I may share relevant details with the appropriate authorities.</li>
+          <li>egal Compliance: Your information will be shared as necessary, to comply with the law.</li>
+        </ul>
+
+        <h2>How I store your personal information:</h2>
+        <ul>
+          <li>Your information is securely stored on the cloud which is password protected. </li>
+          <li>I keep records for a minimum of 7 years as advised by my insurance. After this time period your information will be deleted. </li>
+        </ul>
+
+        <h2>Your rights in regards to your data:</h2>
+        <p>You can visit The Information Comissioner’s Office website for information about your rights regarding GDPR. <a href="https://ico.org.uk/for-the-public/ ">https://ico.org.uk/for-the-public/</a></p>
+
+        <h2>How to complain:</h2>
+        <p>If you have any concerns about my use of your personal information, you
+          can make a complaint to us at <a href="mailto:fiona@wellgroundedcounselling.com">fiona@wellgroundedcounselling.com</a></p>
+
+        <p>You can also complain to the ICO if you are unhappy with how we have
+          used your data.</p>
+        <h2>Information Commissioner’s Office address:</h2>
+        <ul>
+          <li>Wycliffe House</li>
+          <li>Water Lane</li>
+          <li>Wilmslow</li>
+          <li>Cheshire</li>
+          <li>SK9 5AF</li>
+          <li>Helpline number: 0303 123 1113</li>
+          <li>ICO website: <a href="https://www.ico.org.uk"></a>https://www.ico.org.uk</li>
+        </ul>
+
+        <p>I reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If I make material changes to this policy, I will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it</p>
+        <Footer />
+      </div>
+
     </>
   )
 }
