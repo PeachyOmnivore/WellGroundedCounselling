959c1e13119f3c34960d15312e52444159b9dd48
Lukas Dembicki <lad_777@hotmail.com>
2024-03-05 13:30:09 +0000

    feat: my approach page and style

--- src/pages/myApproach/index.jsx
+++ src/pages/myApproach/index.jsx
@@ -1,10 +1,34 @@
+/* eslint-disable react/no-unescaped-entities */
 import "./myApproach.css"
 
-function MyApproach () {
+function MyApproach() {
   return (
-    <>
-    
-    </>
+    <div className="my-approach-container">
+      <div className="my-approach-details">
+        <h1>My Approach</h1>
+        <br />
+        <p>In my counselling practice, I embrace a holistic approach that integrates person-centered and psychodynamic theories, along with elements of experiential approaches such as 'parts work'  and techniques for regulating the nervous system. Recognising that healing and growth are unique journeys, I adjust my methods to meet the individual needs of each client.</p><br />
+
+        <p>
+          I highly value not only engaging in thoughtful conversations with active listening but also delving deeper beyond cognitive understanding.  Our inner world holds richness beyond words. I encourage slowing down to listen to the valuable messages that your body, thoughts, and emotions communicate. These messages tell stories about your past and present feelings and direct the path to healing.</p><br />
+
+        <p>
+          We will look at your emotions and behaviour through a compassionate lens and identify past pain that may be affecting the present.  It is very likely that old wounds are impacting your current emotions, actions and worldview.  While you can’t always control your external circumstances, transforming your internal world will influence how you relate to the external world.</p><br /><br />
+
+        <h2>What you can expect: </h2>
+        <br />
+        <p>
+          I am committed to providing a warm and judgment-free space, where you can express yourself authentically. You get to choose what to bring to each session and what to talk about. I offer various exercises, including visualisations and somatic work, which you can try at your own pace. Between sessions, I may suggest optional "homework" for reflection and integrating insights. This is entirely voluntary and a tool for your personal self-discovery.</p><br /><br />
+
+        <h2>Is well grounded counselling for you?</h2>
+        <br />
+        <p>
+          Do you believe you can grow and have a desire to do so?  Are you curious to get to know yourself better and understand why you do what you do?
+          <br />
+          Are you looking to go beyond simply talking about your problems and want to heal?  Are you willing to be honest with yourself and take ownership of your emotional landscape?</p>
+
+      </div>
+    </div>
   )
 }
 
