c84977a3ddfca85de10647284ee0746473119e77
Lukas Dembicki <lad_777@hotmail.com>
2024-03-05 12:02:47 +0000

    feat: footer added and some style changes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,15 +2,16 @@
 import "./home.css"
 import Header from "../../components/header"
 import Button from "../../components/button"
+import Footer from "../../components/footer"
 import sunsetForest from '../../assets/images/sunsetForest.jpg'
 import { useNavigate } from "react-router-dom"
 
 function Home() {
   const navigate = useNavigate()
-  
+
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="home-grid">
         <div className="img-container">
           <img className="home-img" src={sunsetForest} alt="A sunset in a forest of trees" />
@@ -24,9 +25,10 @@ function Home() {
           <p>
             Life inevitably brings difficulties but my aim is to support people in living with a grounded balance. This means having a regulated nervous system so you can move away from "survival mode", where you impulsively react to life, sometimes in painful ways. Grounding enables you to take ownership of your internal world, empowering you to respond in more helpful ways. It facilitates a connection with your authentic self and promotes meaningful connections with others. While a permanent state of groundedness is unrealistic, counselling can help you by actively addressing barriers and allow you to return to a grounded state with greater ease.
           </p>
-         <Button text={'Get in Touch'} onClick={() => navigate('/contact')}/>
+          <Button text={'Get in Touch'} onClick={() => navigate('/contact')} />
         </section>
       </div>
+      <Footer />
     </div>
   )
 }
