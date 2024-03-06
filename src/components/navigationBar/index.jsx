3369fe94b0935192cb325a665782b6fa27b43a9d
Lukas Dembicki <lad_777@hotmail.com>
2024-03-06 18:00:32 +0000

    feat: completed condition styling for underlines on current page

--- src/components/navigationBar/index.jsx
+++ src/components/navigationBar/index.jsx
@@ -1,16 +1,26 @@
 import "./navigationBar.css"
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 
 function NavigationBar() {
+
+  const location = useLocation();
+  const path = location.pathname;
+
+
   return (
     <div className="navigation_bar">
       <ul>
-        <li><NavLink className="link" to={'/'}><p>Home</p></NavLink></li>
-        <li><NavLink className="link"  to={'about'}><p>About</p></NavLink></li>
-        <li><NavLink className="link"  to={'my-approach'}><p>My approach</p></NavLink></li>
-        <li><NavLink className="link"  to={'services-and-fees'}><p>Services and fees</p></NavLink></li>
-        <li><NavLink className="link"  to={'contact'}><p>Contact</p></NavLink></li>
-        <li><NavLink className="link"  to={'privacy-policy'}><p>Privacy policy</p></NavLink></li>
+        <li><NavLink className={path === "/"?"link currentPage":"link"} to={'/'}><p>Home</p></NavLink></li>
+        <li><NavLink className={path === "/about"?"link currentPage":"link"} to={'/about'}><p>About</p></NavLink></li>
+        <li><NavLink className={path === "/my-approach"?"link currentPage":"link"} to={'/my-approach'}><p>My approach</p></NavLink></li>
+        <li><NavLink className={path === "/services-and-fees"?"link currentPage":"link"} to={'/services-and-fees'}><p>Services and fees</p></NavLink></li>
+        <li><NavLink className={path === "/booking"?"link currentPage":"link"} to={'/booking'}><p>Book a session</p></NavLink></li>
+        <li><NavLink className={path === "/contact"?"link currentPage":"link"} to={'/contact'}><p>Contact</p></NavLink></li>
+        <li><NavLink className={path === "/privacy-policy"?"link currentPage":"link"} to={'/privacy-policy'}><p>Privacy policy</p></NavLink></li>
+      </ul>
+      <ul>
+      <li><NavLink className={path === "/login"?"link currentPage":"link right"} to={'/login'}><p>Login</p></NavLink></li>
+      <li><NavLink className={path === "/register"?"link currentPage":"link right"} to={'/register'}><p>Register</p></NavLink></li>
       </ul>
     </div>
   )
