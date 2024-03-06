7e4fb64d7985597ec3ecdfd467e5b236b56dc138
Lukas Dembicki <lad_777@hotmail.com>
2024-03-06 17:02:24 +0000

    feat: contact page done with form and styles

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,5 +1,6 @@
 import "./contact.css"
 import { useState } from "react"
+import insta from "../../assets/svgs/insta.svg"
 
 function Contact() {
 
@@ -31,48 +32,52 @@ function Contact() {
 
   return (
     <div className="contact-container">
-      <h1>Get in Touch</h1>
-      <p>If you are interested in arranging a session you can either fill in the form below or email me - <a href="mailto:fiona@wellgroundedcounselling.com">fiona@wellgroundedcounselling.com</a></p>
-      <img src="insta" alt="instagram logo" />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name *</label>
-        <input
-          type="text"
-          name="name"
-          required="true"
-          placeholder="Enter your name"
-          value={formData.name}
-          onInput={(event) => {onInput(event)}}
-        />
-        <label htmlFor="email">Email *</label>
-        <input
-          type="email"
-          name="email"
-          required="true"
-          placeholder="Enter your email"
-          value={formData.email}
-          onChange={(event) => { onInput(event) }}
-        />
-        <label htmlFor="subject">Subject</label>
-        <input
-          type="text"
-          name="subject"
-          placeholder="Enter a subject"
-          value={formData.subject}
-          onChange={(event) => { onInput(event) }}
-        />
-        <label htmlFor="message">Message</label>
-        <textarea
-          type="text"
-          name="message"
-          required="true"
-          placeholder="Enter your message"
-          value={formData.message}
-          onChange={(event) => { onInput(event) }}
-        />
-        <button type="submit">Submit</button>
-      </form>
-
+      <section className="form-container">
+        <div className="form-titles">
+        <h1>Get in Touch</h1>
+        <p>If you are interested in arranging a session you can either fill in the form below or email me -<br/><a href="mailto:fiona@wellgroundedcounselling.com">fiona@wellgroundedcounselling.com</a></p>
+        <a href="https://www.instagram.com/wellgroundedcounselling/"><img width="40px" src={insta} alt="Instagram logo" /></a>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name *</label>
+          <input
+            type="text"
+            name="name"
+            required="true"
+            placeholder="Enter your name"
+            value={formData.name}
+            onInput={(event) => { onInput(event) }}
+          />
+          <label htmlFor="email">Email *</label>
+          <input
+            type="email"
+            name="email"
+            required="true"
+            placeholder="Enter your email"
+            value={formData.email}
+            onChange={(event) => { onInput(event) }}
+          />
+          <label htmlFor="subject">Subject</label>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Enter a subject"
+            value={formData.subject}
+            onChange={(event) => { onInput(event) }}
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            type="text"
+            name="message"
+            required="true"
+            placeholder="Enter your message"
+            value={formData.message}
+            onChange={(event) => { onInput(event) }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      <p className="required">* required</p>
+      </section>
     </div>
   )
 }
