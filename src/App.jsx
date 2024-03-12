2b0a3fe24d97cb92575c7e23317f5c97cee9246a
Lukas <lad_777@hotmail.com>
2024-03-12 18:34:28 +0000

    feat: finds Current user

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { get } from './client-functions/index.js'
 import Home from './pages/home'
 import About from './pages/about'
 import MyApproach from './pages/myApproach'
@@ -14,19 +16,39 @@ import Register from './pages/register'
 
 function App() {
 
+  const [currentUser, setCurrentUser] = useState('')
+
+  async function findUser() {
+    try {
+      const foundUser = await get("users/me");
+      setCurrentUser(foundUser);
+      if (!foundUser) {
+        console.log("No logged in user")
+      }
+    } catch (err) {
+      console.error('Error fetching user info:', err);
+    }
+  }
+
+  useEffect(() => {
+    findUser()
+  }, [])
+
+  console.log(currentUser)
+
   return (
     <>
-      <NavigationBar/>
+      <NavigationBar currentUser={currentUser}/>
       <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="my-approach" element={<MyApproach />} />
-            <Route path="services-and-fees" element={<ServicesAndFees />} />
-            <Route path="booking" element={<Booking />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="my-approach" element={<MyApproach />} />
+        <Route path="services-and-fees" element={<ServicesAndFees />} />
+        <Route path="booking" element={<Booking />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </>
   )
