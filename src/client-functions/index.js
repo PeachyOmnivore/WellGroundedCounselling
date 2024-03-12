8e7ff367b5b3c1ab9800d7b2275903bbd04ca3d6
Lukas <lad_777@hotmail.com>
2024-03-12 16:41:44 +0000

    refactor: removed console log and format

--- src/client-functions/index.js
+++ src/client-functions/index.js
@@ -1,36 +1,24 @@
-const serverURL = "http://localhost:3030"
+const serverURL = "http://localhost:3030";
 
 function get() {
-  console.log("hi")
+    console.log("hi");
 }
 
 async function post(bodyData, endpoint) {
-  const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(bodyData)
-  };
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(bodyData),
+    };
 
-  const response = await fetch(`${serverURL}/${endpoint}`, options);
+    const response = await fetch(`${serverURL}/${endpoint}`, options);
 
-  console.log("RESPONSE", response)
-
-  if (!response.ok) {
-    throw new Error(`Error: ${response.status} - ${response.statusText}`);
-  }
-
-  const data = await response.json();
-  return data;
+    const data = await response.json();
+    return data;
 }
 
 function put() {
-  console.log("hi")
+    console.log("hi");
 }
 
-
-
-export {
-  get,
-  post,
-  put,
-}
+export { get, post, put };
