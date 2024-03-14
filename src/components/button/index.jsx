ec66069e159bef8b4556a1276d31035085faf1ea
Lukas <lad_777@hotmail.com>
2024-03-14 20:16:33 +0000

    feat: made button type: button

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -3,7 +3,7 @@ import './button.css'
 
 function Button ({text, onClick, className}) {
   return (
-  <button className={`${className} button` } onClick={onClick}>{text}</button>
+  <button type="button" className={`${className} button` } onClick={onClick}>{text}</button>
   )
 }
 
