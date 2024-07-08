6caec0c768eecb993d02b13d347a1156156ef009
Lukas <lad_777@hotmail.com>
2024-07-08 13:23:59 +0100

    feat: Hamburger menu in working order. Animations next

--- src/components/navigationBar/index.jsx
+++ src/components/navigationBar/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import Hamburger from "../hamburger/hamburger"
 import "./navigationBar.css"
 import { NavLink, useLocation } from "react-router-dom"
 import PopUpCard from '../popupCard/index'
@@ -7,42 +8,53 @@ import { useState } from "react"
 function NavigationBar({ currentUser, setCurrentUser, currentlyAdmin }) {
 
   const [showPopup, setShowPopup] = useState(false)
-  const [showNav, setShowNav] = useState(false)
+  const [hamburgerOpen, setHamburgerOpen] = useState(true)
   const location = useLocation();
   const path = location.pathname;
 
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen)
+  }
+
   return (
-    <div className="navigation_bar">
-      <ul>
-        <li><NavLink className={path === "/" ? "link currentPage" : "link"} to={'/'}><p>Home</p></NavLink></li>
-        <li><NavLink className={path === "/about" ? "link currentPage" : "link"} to={'/about'}><p>About</p></NavLink></li>
-        <li><NavLink className={path === "/my-approach" ? "link currentPage" : "link"} to={'/my-approach'}><p>My approach</p></NavLink></li>
-        <li><NavLink className={path === "/services-and-fees" ? "link currentPage" : "link"} to={'/services-and-fees'}><p>Services and fees</p></NavLink></li>
-        {currentUser && <li><NavLink className={path === "/booking" ? "link currentPage" : "link"} to={'/booking'}>{!currentlyAdmin? <p>Book a session</p> : <p>Add / Remove Sessions</p>}</NavLink></li>}
-        <li><NavLink className={path === "/contact" ? "link currentPage" : "link"} to={'/contact'}><p>Contact</p></NavLink></li>
-        <li><NavLink className={path === "/privacy-policy" ? "link currentPage" : "link"} to={'/privacy-policy'}><p>Privacy policy</p></NavLink></li>
-      </ul>
-      <ul>
-        {currentUser.foundUser ? (
-          <>
-            <li><a onClick={() => setShowPopup(!showPopup)} className="link right"><p>{`Welcome ${currentUser.foundUser.firstName}`}</p></a></li>
-            {showPopup ? (
-              <PopUpCard>
-                {!currentlyAdmin ? <NavLink to={'/booking'}><p>Book a session</p></NavLink> : <NavLink className={"clientNav"} to={'/clients'}><p>Clients</p></NavLink>}
-                <NavLink to={'/myProfile'}><p>Profile / Bookings</p></NavLink>
-                <NavLink to={'/'} onClick={() => { localStorage.removeItem("token"); setCurrentUser('') }}><p>Logout</p></NavLink>
-              </PopUpCard>)
-              : (null)}
-          </>
-        ) : (
-          <>
-            <li><NavLink className={path === "/login" ? "link currentPage" : "link right"} to={'/login'}><p>Login</p></NavLink></li>
-            <li><NavLink className={path === "/register" ? "link currentPage" : "link right"} to={'/register'}><p>Register</p></NavLink></li>
-          </>
-        )
-        }
-      </ul>
-    </div>
+    <>
+      {hamburgerOpen ? (<li className="hamburger-nav navigation_bar" onClick={toggleHamburger}><Hamburger /></li>) : (
+        <div className="navigation_bar">
+          <ul>
+          <li className="hamburger-nav navigation_bar" onClick={toggleHamburger}><Hamburger /></li>
+            <li><NavLink className={path === "/" ? "link currentPage" : "link"} to={'/'}><p>Home</p></NavLink></li>
+            <li><NavLink className={path === "/about" ? "link currentPage" : "link"} to={'/about'}><p>About</p></NavLink></li>
+            <li><NavLink className={path === "/my-approach" ? "link currentPage" : "link"} to={'/my-approach'}><p>My approach</p></NavLink></li>
+            <li><NavLink className={path === "/services-and-fees" ? "link currentPage" : "link"} to={'/services-and-fees'}><p>Services and fees</p></NavLink></li>
+            {currentUser && <li><NavLink className={path === "/booking" ? "link currentPage" : "link"} to={'/booking'}>{!currentlyAdmin ? <p>Book a session</p> : <p>Add / Remove Sessions</p>}</NavLink></li>}
+            <li><NavLink className={path === "/contact" ? "link currentPage" : "link"} to={'/contact'}><p>Contact</p></NavLink></li>
+            <li><NavLink className={path === "/privacy-policy" ? "link currentPage" : "link"} to={'/privacy-policy'}><p>Privacy policy</p></NavLink></li>
+
+          </ul>
+          <ul>
+            {currentUser.foundUser ? (
+              <>
+                <li><a onClick={() => setShowPopup(!showPopup)} className="link right"><p>{`Welcome ${currentUser.foundUser.firstName}`}</p></a></li>
+                {showPopup ? (
+                  <PopUpCard>
+                    {!currentlyAdmin ? <NavLink to={'/booking'}><p>Book a session</p></NavLink> : <NavLink className={"clientNav"} to={'/clients'}><p>Clients</p></NavLink>}
+                    <NavLink to={'/myProfile'}><p>Profile / Bookings</p></NavLink>
+                    <NavLink to={'/'} onClick={() => { localStorage.removeItem("token"); setCurrentUser('') }}><p>Logout</p></NavLink>
+                  </PopUpCard>)
+                  : (null)}
+              </>
+            ) : (
+              <>
+                <li><NavLink className={path === "/login" ? "link currentPage" : "link right"} to={'/login'}><p>Login</p></NavLink></li>
+                <li><NavLink className={path === "/register" ? "link currentPage" : "link right"} to={'/register'}><p>Register</p></NavLink></li>
+              </>
+            )
+          }
+          </ul>
+        </div>
+      )
+      }
+    </>
   )
 }
 
